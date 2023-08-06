0f47a8c06d9f17d8921333e6c959cc7260e4c95b
MarcinDraus <marcin.drauss@gmail.com>
2023-08-06 19:21:09 +0200

    fetch

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,12 +2,18 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import { useCallback } from 'react';
+import { useCallback, } from 'react';
+
 
 const WeatherBox = props => {
 
   const handleCityChange = useCallback(city => {
     console.log('cityName', city)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6d7dc5256560d9b30ad0ff54a39a7a90&units=metric`)
+   .then(res => res.json())
+   .then(data => {
+     console.log(data);
+   });
   }, []);
 
   return (
